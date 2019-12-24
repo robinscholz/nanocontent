0978af9337cc6e2f1f4cd3a23e66c5c79b4da1af
robinscholz <dev@studioscholz.info>
2019-12-24 17:02:01 +0100

    es6 import/export

--- readme.md
+++ readme.md
@@ -1,16 +1,3 @@
-<h1 align="center">nanocontent</h1>
-
-<div align="center">
-  <a href="https://nodejs.org/api/documentation.html#documentation_stability_index">
-    <img src="https://img.shields.io/badge/stability-experimental-orange.svg?style=flat-square" alt="Stability" />
-  </a>
-  <a href="https://www.npmjs.com/package/nanocontent">
-    <img src="https://img.shields.io/npm/v/nanocontent.svg?style=flat-square" alt="NPM version" />
-  </a>
-</div>
-
-<br />
-
 **work in progress...**
 
 Introducing flat content state. Read directories of content into an object.
@@ -72,7 +59,7 @@ Each directory becomes a path containing a sub-object of the content in your tex
 }
 ```
 
-Map over the object keys to add routes to a router, then pass the content object. Huzzah.
+Map over the object keys to add routes to a router, then pass the content object.
 
 ## API
 
@@ -106,20 +93,3 @@ Substitute `gray-matter` with your own parser. Must be able to transform a plain
 
 Remove part of the `url` for pretty printing. For example, if your content lives in `/content`, but you don’t want to prefix all of your `urls` with `/content`, use `parent` to clean it up. Value can be a string or boolean. If `true`, the `path` of your initial call is used.
 
-## Transform
-
-```
-browserify -t nanocontent/transform
-```
-
-A browserify transform located at `nanocontent/transform` is included to staticly inline the module output.
-
-## Example
-
-A demo site is included. Open the `nanocontent/example` dir and `npm install`. The example uses [Bankai](https://github.com/choojs/bankai). Run `npm start` to spin up a Bankai server and mess around. Run `bankai build` to build a fully static site.
-
-## Todo
-
-- [ ] Tests
-- [ ] Async callback/promise fallback
-- [x] Modularize read function for JSON/Smarkt/Custom
--- test.js
+++ test.js
@@ -1,38 +1,40 @@
 const test = require('ava')
 const nanocontent = require('.')
 
-test('readPageSync works', function (t) {
+test('readPageSync works', function(t) {
   var page = nanocontent.readPageSync('example/content/about')
   t.is(page.data.title, 'About')
   t.is(page.data.view, 'custom')
 })
 
-test('readPage works', async function (t) {
+test('readPage works', async function(t) {
   var page = await nanocontent.readPage('example/content/about')
   t.is(page.data.title, 'About')
   t.is(page.data.view, 'custom')
 })
 
-test('readPageSync and readPage outputs are the same', async function (t) {
+test('readPageSync and readPage outputs are the same', async function(t) {
   var syncPage = nanocontent.readPageSync('example/content/about')
   var asyncPage = await nanocontent.readPage('example/content/about')
   t.deepEqual(syncPage, asyncPage)
 })
 
-test('readSiteSync works', function (t) {
+test('readSiteSync works', function(t) {
   var site = nanocontent.readSiteSync('example/content')
   t.is(site['/example/content'].data.title, 'Example')
   t.is(site['/example/content/about'].data.title, 'About')
 })
 
-test('readSiteSync and readSite outputs are the same', async function (t) {
+test('readSiteSync and readSite outputs are the same', async function(t) {
   var syncSite = nanocontent.readSiteSync('example/content')
   var asyncSite = await nanocontent.readSite('example/content')
   t.deepEqual(syncSite, asyncSite)
 })
 
-test('readSiteSync and readSite outputs are the same with parent option', async function (t) {
+test('readSiteSync and readSite outputs are the same with parent option', async function(t) {
   var syncSite = nanocontent.readSiteSync('example/content', { remove: true })
-  var asyncSite = await nanocontent.readSite('example/content', { remove: true })
+  var asyncSite = await nanocontent.readSite('example/content', {
+    remove: true,
+  })
   t.deepEqual(syncSite, asyncSite)
 })
